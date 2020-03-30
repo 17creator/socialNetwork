4cff3668ced305d03ca184c77e4079233364b2ca
17creator <nuttertools89nuttertools89>
2020-03-30 23:12:56 +0600

    added react files

---
+++ src/components/Content.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="image-content"/>
+            </div>
+            <div>
+                ava + discription
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img className='logo' src="https://upload.wikimedia.org/wikipedia/ru/0/0f/Mortal_kombat_logo.png" alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
