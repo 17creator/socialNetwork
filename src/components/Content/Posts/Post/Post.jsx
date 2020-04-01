38f1e4c36c9813bfb47111f72b212661b550833d
17creator <nuttertools89nuttertools89>
2020-04-01 23:50:14 +0600

    16. added props to project

--- src/components/Content/Posts/Post/Post.jsx
+++ src/components/Content/Posts/Post/Post.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img className={s.img} src="https://cr-mp.ru/forum/uploads/monthly_2019_07/2.thumb.png.4bf6d70498bfc47585ccc6bc94d3390b.png" alt="userAva"/>
-            Post1
+            {props.message}
+            <div>
+                <span>Like {props.countLike}</span>
+            </div>
         </div>
     );
 }
--- src/components/Content/Posts/Posts.jsx
+++ src/components/Content/Posts/Posts.jsx
@@ -5,12 +5,9 @@ import Post from "./Post/Post";
 const Posts = () => {
     return (
         <div>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post message='Hi, how old are you?' countLike='40'/>
+            <Post message='Its my first post' countLike='20' />
+
         </div>
 
     );
